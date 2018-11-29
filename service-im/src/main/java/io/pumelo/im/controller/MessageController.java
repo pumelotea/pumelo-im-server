97f5435f953a7fdb5b961537abc93e484b48fa71
pumelotea <535553297@qq.com>
2018-11-29 15:54:27 +0800

    实现基于MYSQL的消息持久化

--- service-im/src/main/java/io/pumelo/im/controller/MessageController.java
+++ service-im/src/main/java/io/pumelo/im/controller/MessageController.java
@@ -1,9 +1,32 @@
 package io.pumelo.im.controller;
 
+import io.pumelo.authorizion.AuthFilter;
+import io.pumelo.common.web.ApiResponse;
+import io.pumelo.data.im.entity.MessageEntity;
+import io.pumelo.data.im.vo.message.MessageListVo;
+import io.pumelo.im.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MessageController {
+    @Autowired
+    private MessageService messageService;
 
+    @AuthFilter
+    @GetMapping("/message_preview")
+    public ApiResponse<List<MessageEntity>> getOfflineMessagePreview(){
+        return messageService.getOfflineMessagePreview();
+    }
+
+    @AuthFilter
+    @GetMapping("/offline_message/{uid}")
+    public ApiResponse<MessageListVo> getOffineMessage(@PathVariable String uid){
+        return messageService.getOffineMessage(uid);
+    }
 
 }
